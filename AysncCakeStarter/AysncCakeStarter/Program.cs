ecddb9759fa575b6b52ec20b7d4a88005b09d47d
Dæmon <xdamon@hotmail.co.uk>
2022-12-12 12:00:51 +0000

    Completed AsyncCake project

--- AysncCakeStarter/AysncCakeStarter/Program.cs
+++ AysncCakeStarter/AysncCakeStarter/Program.cs
@@ -1,44 +1,50 @@
 ﻿using System;
 using System.Threading;
+using System.Threading.Tasks;
 
 namespace AysncCake
 {
     class Program
     {
-        public static void Main(string[] args)
-        {
+        public static async Task Main(string[] args) {
             Console.WriteLine("Welcome to my birthday party");
-            HaveAParty();
+            await HaveAPartyAsync();
             Console.WriteLine("Thanks for a lovely party");
             Console.ReadLine();
         }
 
-        private static void HaveAParty()
-        {
+        private static async Task HaveAPartyAsync() {
             var name = "Cathy";
-            var cake = BakeCake();
+            var cakeTask = BakeCakeAsync();
+            var pizzaTask = OrderPizzaAsync();
             PlayPartyGames();
             OpenPresents();
+            await pizzaTask;
+            var cake = await cakeTask;
             Console.WriteLine($"Happy birthday, {name}, {cake}!!");
         }
 
-        private static Cake BakeCake()
-        {
+        private static async Task<Cake> BakeCakeAsync() {
             Console.WriteLine("Cake is in the oven");
-            Thread.Sleep(TimeSpan.FromSeconds(5));
+            await Task.Delay(TimeSpan.FromSeconds(5));
             Console.WriteLine("Cake is done");
             return new Cake();
         }
 
-        private static void PlayPartyGames()
-        {
+        private static async Task OrderPizzaAsync() {
+            Console.WriteLine("Pizza has been ordered");
+            await Task.Delay(TimeSpan.FromSeconds(4));
+            Console.WriteLine("Pizza delivered");
+            Console.WriteLine("Eating the pizza...");
+        }
+
+        private static void PlayPartyGames() {
             Console.WriteLine("Starting games");
             Thread.Sleep(TimeSpan.FromSeconds(2));
             Console.WriteLine("Finishing Games");
         }
 
-        private static void OpenPresents()
-        {
+        private static void OpenPresents() {
             Console.WriteLine("Opening Presents");
             Thread.Sleep(TimeSpan.FromSeconds(1));
             Console.WriteLine("Finished Opening Presents");
@@ -47,9 +53,9 @@ namespace AysncCake
 
     public class Cake
     {
-        public override string ToString()
-        {
+        public override string ToString() {
             return "Here's a cake";
         }
     }
 }
+
